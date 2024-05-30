56c2b5f17739778d2702c322c7876091711ccb11
Renan <renan.castro@dashlane.com>
2024-05-30 09:04:29 +0100

    added Tasks.jsx

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,26 +1,29 @@
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Tasks from "./assets/tasks.json"
+import tasksData from "./assets/tasks.json";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [dataState, setDataState] = useState(tasksData);
   return (
     <>
-    <div className="App">
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
 
-      <header>
-        <Navbar />
-      </header>
+        <br />
 
-      <br />
+        <main className="main">
+          <button className="main-button">To-do List</button>
+        </main>
 
-      <main className="main">
-        <button className="main-button">To-do List</button>
-      </main>
-
-      <br />
-      <Footer />
+        <br />
+        <Footer />
       </div>
+      <Tasks props={dataState} />
     </>
   );
 }
---
+++ react-project/src/components/Tasks.jsx
@@ -0,0 +1,10 @@
+const Tasks = ({props}) => {
+  return (
+    <div>
+   
+
+    </div>
+  );
+};
+export default Tasks;
+
