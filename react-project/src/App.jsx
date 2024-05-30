ffad03e4aa61571d18594a14c238c8a2577a2cfe
Renan <renan.castro@dashlane.com>
2024-05-30 19:27:14 +0100

    more routing

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import tasksData from "./assets/tasks.json";
 import Tasks from "./components/Tasks";
+import DashBoard from "./pages/DashBoard";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import ItemDetails from "./pages/ItemDetails";
 import { useState } from "react";
 
 function App() {
@@ -22,13 +27,19 @@ function App() {
     <>
       <div className="App">
         <header>
-          <Navbar />
+          <Routes>
+            <Route path="/" element={<DashBoard />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/itemDetails" element={<ItemDetails />} />
+            <Route path="*" element={<NotFound />} />
+            <Navbar />
+          </Routes>
         </header>
         <br />
 
         <main className="main">
           <button className="main-button">To-do List</button>
-          <tasks dataState = {dataState} setDataState = {setDataState} />
+          <tasks dataState={dataState} setDataState={setDataState} />
           <Tasks data={dataState} deleteToDo={deleteToDo} />
         </main>
 
