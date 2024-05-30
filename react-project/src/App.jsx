6c373de6f7c94960883250a5c03fdd61cff6b109
Zeina <zeinamershed1@gmail.com>
2024-05-30 17:01:22 +0200

    tasks

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 import tasksData from "./assets/tasks.json";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
@@ -13,11 +14,12 @@ function App() {
         <header>
           <Navbar />
         </header>
-
+        <Sidebar />
         <br />
 
         <main className="main">
           <button className="main-button">To-do List</button>
+          <tasks dataState = {dataState} setDataState = {setDataState} />
         </main>
 
         <br />
---
+++ react-project/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    
+      <div className="sidebar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+    
+  );
+}
+
+export default Sidebar
