a7297a3368e0556d98d07dba6f071938bdabecbb
Zeina <zeinamershed1@gmail.com>
2024-05-31 17:24:48 +0200

    githublink

--- react-project/src/components/Footer.jsx
+++ react-project/src/components/Footer.jsx
@@ -1,13 +1,26 @@
-import icon from "../assets/images/icon.png"
+import githublogo from "../assets/images/githublogo.jpg";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={icon} alt="" />
+        <button> 
+         <a href="https://github.com/zeinamershed/mini-project-react">
+            <img src={githublogo} alt="github-logo"/> 
+            
+            </a>
+        </button>
+        
+
+      <h4>Renan and Zeina</h4>
       
     </div>
   )
 }
 
 export default Footer
+
+
+
+
+
 
